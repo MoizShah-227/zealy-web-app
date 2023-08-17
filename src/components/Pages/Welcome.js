4be0a2c2499543a8776685a5312ecf4ad1744111
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-08-17 10:33:36 +0500

    Not Done yet!!

--- src/components/Pages/Welcome.js
+++ src/components/Pages/Welcome.js
@@ -34,11 +34,31 @@ const Welcome = () => {
         <div className='mt-4 nav-menu'>
          
             <a><button className='btn text-left'>Quests</button></a><br/>
-            <a><button  className='btn text-left'>Leaderboard</button></a>
-
-         
+            <a><button  className='btn text-left'>Leaderboard</button></a>         
         </div>
-      </div>        
+      </div>  
+
+      <div className='welcome-left-side container'>
+        <div className='row'>
+          <div className='col-lg-2 col-md-12 '>
+          <img  className='left-side-img px-2' width={200} src='https://ugc.production.linktr.ee/70MKy00yQESJzYTeawbx_zealy%20Logo_Mark_White.png'href ="/" />
+          </div>
+          <div className='col-lg-6 col-md-12 '>
+            <h1>Welcome To Zealy</h1>
+            <p>You're at the right place. Choose your path forward ✨</p>
+            <div className='icons mx-3'>
+              <a><i class="bi bi-link-45deg m-2"></i></a>
+              <a><i class="bi bi-discord m-2"></i></a>
+            </div>
+          </div>
+
+          
+        </div>
+        <div className='welcome-btn mt-3'>
+          <a><button className='btn2 m-2'>👋 Find your Zeal</button></a>
+          <a><button className='btn2 m-2'>🔥 Prepare for Lift-off </button></a>
+          </div>
+      </div>      
     </div>
   )
 }

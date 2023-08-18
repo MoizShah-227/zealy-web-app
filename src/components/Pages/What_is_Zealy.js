444e568e36f32b9c1b779db9a8c1f938d3405c9f
MoizShah-227 <103181576+MoizShah-227@users.noreply.github.com>
2023-08-18 10:35:45 +0500

    TRY

--- src/components/Pages/What_is_Zealy.js
+++ src/components/Pages/What_is_Zealy.js
@@ -4,11 +4,14 @@ import Zealy_Doc from './Zealy_Doc'
 const What_is_Zealy = () => {
   return (
     <div className='doc-container'>        
+        <div className='zealy-container'>
         <Zealy_Doc/>
-  
-    
-      
-
+        <div className='contain-zealy-data container'>
+          <div className='upper-img'>
+          <img src='https://1505557047-files.gitbook.io/~/files/v0/b/gitbook-x-prod.appspot.com/o/spaces%2FQvTIrayABkLCC3fDPMH6%2Fuploads%2FsCeEFJHcP7dDChWom1OT%2FTeam%20banner.png?alt=media&token=a469b401-7fee-41fd-a54d-631913eb05d9'/>
+          </div>
+        </div>
+        </div>
     </div>
   )
 }
